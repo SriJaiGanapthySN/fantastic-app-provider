4b14d77fdc5d44e8fda21a5361c5be42ed20fc38
VaidantSharma <vaidantsharma47@gmail.com>
2025-04-25 21:51:04 +0530

    Added Images to crousel

--- lib/OnBoarding/Widgets/socialList.dart
+++ lib/OnBoarding/Widgets/socialList.dart
@@ -6,10 +6,10 @@ class SocialList {
 }
 
 List<SocialList> socialMediaList = [
-  SocialList(imageAdd: 'assets/images/image0.png', text: "Social Media/\nOnline ads"),
-  SocialList(imageAdd: 'assets/images/image0.png', text: "Friend or Family"),
-  SocialList(imageAdd: 'assets/images/image0.png', text: "Youtube Ad"),
-  SocialList(imageAdd: 'assets/images/image0.png', text: "Podcast Ad"),
-  SocialList(imageAdd: 'assets/images/image0.png', text: "Online Publication"),
-  SocialList(imageAdd: 'assets/images/image0.png', text: "Other"),
+  SocialList(imageAdd: 'assets/images/socialMedia.png', text: "Social Media/\nOnline ads"),
+  SocialList(imageAdd: 'assets/images/family.png', text: "Friend or Family"),
+  SocialList(imageAdd: 'assets/images/youtube.png', text: "Youtube Ad"),
+  SocialList(imageAdd: 'assets/images/podcast.png', text: "Podcast Ad"),
+  SocialList(imageAdd: 'assets/images/online.png', text: "Online Publication"),
+  SocialList(imageAdd: 'assets/images/other.png', text: "Other"),
 ];
